d2e571788420266d4385cafcb3935470e611cdee
KhamzaKuranbayev <khamza@nightwell-logistics.com>
2022-06-26 12:17:22 +0500

    init commit

---
+++ src/main/java/AppDemo.java
@@ -0,0 +1,10 @@
+/**
+ * Author: khamza@nightwell-logistics.com
+ * Date: 6/26/2022
+ * Time: 12:15 PM
+ */
+public class AppDemo {
+    public static void main(String[] args) {
+        /// TODO: 6/26/2022 your code here
+    }
+}

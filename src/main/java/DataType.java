a3df2bcb972b85ced3bd718a6c68c6705025ba05
KhamzaKuranbayev <khamza@nightwell-logistics.com>
2022-07-01 21:33:15 +0500

    added some changes

---
+++ src/main/java/DataType.java
@@ -0,0 +1,16 @@
+/**
+ * Author: khamza@nightwell-logistics.com
+ * Date: 7/1/2022
+ * Time: 9:29 PM
+ */
+public class DataType {
+    public static void main(String[] args) {
+        // primitive and non-primitive
+
+        // primitive
+        //   Integer, Double,  Short, Boolean, Byte, Long, Float, Character
+        //    int       double short  boolean  byte  long float  char
+        Integer age = 35;
+        int age2 = 35;
+    }
+}
